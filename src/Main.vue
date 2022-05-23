c91b9f2eda588ef7c0ec21bc6f53c7f317d368c2
babuchirra12 <babuchirragithub@gmail.com>
2022-05-24 00:30:39 +0530

    initial commit

---
+++ src/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="welcome">Welcome to our vue site</div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav-bar">
+    <div class="title">BasicsAdmin</div>
+    <ul class="top-nav">        
+        <li>
+        <a href='/'>Logout</a>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
